96c68dca9b38a4a06d4460301501382966a02c20
valkov <valkovjoro@gmail.com>
2018-11-03 10:37:50 -0400

    Creating Reader Class

---
+++ Reader.py
@@ -0,0 +1,43 @@
+import numpy as np
+import cv2
+import tensorflow as tf
+
+class Reader():
+    def __init__(self, graph, **kwargs):
+        super(Reader, self).__init__(**kwargs)
+        self.graph = self.__load_graph(graph)
+        self.config = tf.ConfigProto(allow_soft_placement=True)
+        self.session = tf.Session(graph=self.graph, config=self.config)
+
+
+    def __load_graph(self, path_to_frozen_graph):
+        # Load a (frozen) Tensorflow model into memory.
+        detection_graph = tf.Graph()
+        with detection_graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(path_to_frozen_graph, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+
+        return detection_graph
+
+    # Takes image as an array
+    def read(self, image):
+        # We access the input and output nodes
+        x = self.session.graph.get_tensor_by_name('input_image_as_bytes:0')
+        prediction = self.session.graph.get_tensor_by_name('prediction:0')
+        probability = self.session.graph.get_tensor_by_name('probability:0')
+        # Convert image to png
+        #image = cv2.imread(image, 0) # For testing
+        success, encoded_img = cv2.imencode('.png', image)
+        image = encoded_img.tobytes()
+
+        # Expand Dims
+        image = np.expand_dims(image, axis=0)
+        # Run session on image
+        (prediction_out, probability_out) = self.session.run([prediction, probability], feed_dict={
+            x: image
+        })
+
+        return prediction_out, probability_out
